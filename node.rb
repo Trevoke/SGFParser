f9172b0e1362708b46ef52b848686727760876d7
Aldric Giacomoni <aldric@trevoke.net>
2009-09-17 00:31:07 -0400

    Adding the first, still broken version of the code.

---
+++ node.rb
@@ -0,0 +1,31 @@
+class SGFNode
+
+  attr_accessor :next, :previous, :depth, :properties
+
+  def initialize args
+    @next = []
+    add_next args[:next] if !args[:next].nil?
+    @previous = args[:previous] rescue nil
+    @depth = args[:depth] rescue -1
+    @properties = Hash.new
+    @properties.merge args[:properties] if !args[:properties].nil?
+
+  end
+
+  def add_next node
+    @next << node
+  end
+
+  def add_properties hash
+    #puts
+    #pp hash
+    #puts
+    hash.each do |key, value|
+      ident = key.to_sym 
+      #pp ident, value
+      @properties[ident] ||= [] 
+      @properties[ident].concat value 
+    end
+  end
+
+end
---
+++ tree.rb
@@ -0,0 +1,102 @@
+require 'node'
+#TODO It's still just a damn list! Have to make it into a tree!
+class SGFTree
+
+  attr_accessor :root, :sgf
+
+  def initialize string = ""
+    @root = SGFNode.new :depth => -1, :previous => nil
+    @sgf = ""
+    File.open(string, 'r') { |f| @sgf = f.read }
+    parse unless @sgf.empty?
+  end
+  
+  def each
+    node_list = []
+  end
+
+  private
+
+  def parse
+    # Getting rid of newlines.
+    @sgf.gsub! "\\\\n\\\\r", ""
+    @sgf.gsub! "\\\\r\\\\n", ""
+    @sgf.gsub! "\\\\r", ""
+    @sgf.gsub! "\\\\n", ""
+    @sgf.gsub! "\n", " "
+    #previous = @root # Initialize : first is the root, then...
+    current = @root
+    depth = 0 # Clearly the next node's depth is 0...
+    identprop = false # We are not in the middle of an identprop value.
+    content = Hash.new # Hash holding all the properties
+    param, property = "", "" # Variables holding the properties
+    end_of_a_series = false # To keep track of params with multiple properties
+
+    # Simplest way is probably to iterate through every character, and use
+    # a case scenario
+    @sgf.each_char do |char|
+      case char
+      when '('  # Opening a new branch
+        identprop ? (property += char) : (depth += 1)
+
+      when ')' # Closing a branch
+
+        if identprop
+          property += char
+        else
+          # back to correct node. Should just be one.
+          #depth -= 1 # Wrong thinking? Have it removed?
+          current = current.previous until current.depth == depth
+        end
+
+      when ';' # Opening a new node
+        if identprop
+          property += char
+        else
+          # Make the current node the old node, make new node, store data
+          previous = current
+          current = SGFNode.new :previous => previous, :depth => depth
+          previous.add_properties content
+          previous.add_next current
+          param, property = "", ""
+          content.clear
+        end
+
+      when '[' # Open comment?
+        if identprop
+          property += char
+        else # If we're not inside a comment, then now we are.
+          identprop = true
+          end_of_a_series = false
+        end
+
+      when ']' # Close comment
+        end_of_a_series = true # Maybe end of a series of comments.
+        identprop = false # That's our cue to close a comment.
+        content[param] ||= []
+        content[param] << property
+        property = ""
+      when '\]'
+        identprop ? (property += char) : param += char
+      when ' '
+        identprop ? (property += char) : next
+
+      else
+        # Well, I guess it's "just" a character after all.
+        if end_of_a_series
+          end_of_a_series = false
+          param, property = "", ""
+        end
+        identprop ? (property += char) : param += char
+      end
+
+    end
+  end
+
+  def method_missing method_name, *args
+    output = @root.next[0].properties[method_name]
+    super if output.nil?
+    output
+  end
+
+end
