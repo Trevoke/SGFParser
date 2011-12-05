50f657e5272b1730b6686ae14d8a262d7bab3e1a
Aldric Giacomoni <trevoke@gmail.com>
2011-12-05 14:19:22 -0500

    rename executable for sgf indenter : sgf_indent

--- bin/sgf_indent.rb
+++ bin/sgf_indent.rb
@@ -3,6 +3,11 @@
 
 require '../lib/sgf'
 
+if ARGV.size != 2
+  puts "Usage:\n sgf_indent source.sgf destination.sgf"
+  exit
+end
+
 parser = SGF::Parser.new
 tree = parser.parse ARGV[0]
 tree.save ARGV[1]
