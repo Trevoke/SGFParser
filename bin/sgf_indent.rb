230b79bee32c667e01c8e8d4ee1f7c53c8e3cf45
Aldric Giacomoni <trevoke@gmail.com>
2016-04-23 16:38:03 -0400

    sgf_indent tells shell to use Ruby

--- bin/sgf_indent.rb
+++ bin/sgf_indent.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 #Thankfully, whitespace doesn't matter in SGF, unless you're inside a comment.
 #This makes it possible to indent an SGF file and let it be somewhat human-readable.
 
@@ -5,7 +6,7 @@ require '../lib/sgf'
 
 if ARGV.size != 2
   puts "Usage:\n sgf_indent source.sgf destination.sgf"
-  exit
+  exit 1
 end
 
 parser = SGF::Parser.new
