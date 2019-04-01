dd4c4f435bd21f03dcd7a35d080c703852b0554e
Aldric Giacomoni <trevoke@gmail.com>
2019-03-31 22:21:04 -0400

    more refactors

--- lib/sgf/parsing_tokens.rb
+++ lib/sgf/parsing_tokens.rb
@@ -1,43 +1,44 @@
 # frozen_string_literal: true
+module SGF
+  class IdentityToken
+    def still_inside?(char, _token_so_far, _sgf_stream)
+      char != '['
+    end
 
-class SGF::IdentityToken
-  def still_inside?(char, _token_so_far, _sgf_stream)
-    char != '['
+    def transform(token)
+      token.delete "\n"
+    end
   end
 
-  def transform(token)
-    token.delete "\n"
-  end
-end
+  class CommentToken
+    def still_inside?(char, token_so_far, _sgf_stream)
+      char != ']' || (char == ']' && token_so_far[-1..-1] == '\\')
+    end
 
-class SGF::CommentToken
-  def still_inside?(char, token_so_far, _sgf_stream)
-    char != ']' || (char == ']' && token_so_far[-1..-1] == '\\')
-  end
-
-  def transform(token)
-    token.gsub '\\]', ']'
-  end
-end
-
-class SGF::MultiPropertyToken
-  def still_inside?(char, _token_so_far, sgf_stream)
-    return true if char != ']'
-
-    sgf_stream.peek_skipping_whitespace == '['
+    def transform(token)
+      token.gsub '\\]', ']'
+    end
   end
 
-  def transform(token)
-    token.gsub('][', ',').split(',')
-  end
-end
+  class MultiPropertyToken
+    def still_inside?(char, _token_so_far, sgf_stream)
+      return true if char != ']'
 
-class SGF::GenericPropertyToken
-  def still_inside?(char, _token_so_far, _sgf_stream)
-    char != ']'
+      sgf_stream.peek_skipping_whitespace == '['
+    end
+
+    def transform(token)
+      token.gsub('][', ',').split(',')
+    end
   end
 
-  def transform(token)
-    token
+  class GenericPropertyToken
+    def still_inside?(char, _token_so_far, _sgf_stream)
+      char != ']'
+    end
+
+    def transform(token)
+      token
+    end
   end
 end
--- lib/sgf/stream.rb
+++ lib/sgf/stream.rb
@@ -2,52 +2,54 @@
 
 require 'stringio'
 
-class SGF::Stream
-  attr_reader :stream
+module SGF
+  class Stream
+    attr_reader :stream
 
-  def initialize(sgf, error_checker)
-    sgf = sgf.read if sgf.instance_of?(File)
-    sgf = File.read(sgf) if File.exist?(sgf)
-    error_checker.check_for_errors_before_parsing sgf
-    @stream = StringIO.new clean(sgf), 'r'
-  end
-
-  def eof?
-    stream.eof?
-  end
-
-  def next_character
-    !stream.eof? && stream.sysread(1)
-  end
-
-  def read_token(format)
-    property = ''
-    while (char = next_character) && format.still_inside?(char, property, self)
-      property += char
+    def initialize(sgf, error_checker)
+      sgf = sgf.read if sgf.instance_of?(File)
+      sgf = File.read(sgf) if File.exist?(sgf)
+      error_checker.check_for_errors_before_parsing sgf
+      @stream = StringIO.new clean(sgf), 'r'
     end
-    format.transform property
-  end
 
-  def peek_skipping_whitespace
-    while char = next_character
-      next if char[/\s/]
-
-      break
+    def eof?
+      stream.eof?
     end
-    rewind if char
-    char
-  end
 
-  private
+    def next_character
+      !stream.eof? && stream.sysread(1)
+    end
 
-  def rewind
-    stream.pos -= 1
-  end
+    def read_token(format)
+      property = ''
+      while (char = next_character) && format.still_inside?(char, property, self)
+        property += char
+      end
+      format.transform property
+    end
 
-  def clean(sgf)
-    sgf.gsub('\\\\n\\\\r', '')
-       .gsub('\\\\r\\\\n', '')
-       .gsub('\\\\r', '')
-       .gsub('\\\\n', '')
+    def peek_skipping_whitespace
+      while char = next_character
+        next if char[/\s/]
+
+        break
+      end
+      rewind if char
+      char
+    end
+
+    private
+
+    def rewind
+      stream.pos -= 1
+    end
+
+    def clean(sgf)
+      sgf.gsub('\\\\n\\\\r', '')
+          .gsub('\\\\r\\\\n', '')
+          .gsub('\\\\r', '')
+          .gsub('\\\\n', '')
+    end
   end
 end
